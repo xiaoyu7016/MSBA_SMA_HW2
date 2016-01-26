fd6bfa3cfac8f4adb831e657199437c92b7c6d30
xiaoyu7016 <wangyuw@utexas.edu>
2016-01-25 22:15:38 -0600

    Intial Script

---
+++ hw2.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 25 22:10:41 2016
+
+@author: Yuwen Wang
+"""
+####################
+# Authorization
+####################
+api_key = "2lzEpNDwz9ieHhr738sXX8Avl"
+api_secret = "z3srlvggHvhCqIBSMnKRbn5OY36B2Z58299ipfKtuRXMqwI4rj"
+
+token_key = "3345186255-aW2FoWb6TP3gFgWRLBIbI6OJ6SUCYkYJEv5fZBR"
+token_secret = "F3SOeX3nVZPhaePFzNQFX3yjgxWnFrMw77aH2946kDFcK"
+
+owner = "ywwang1993"
+owner_id = "3345186255"
